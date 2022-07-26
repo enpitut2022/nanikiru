b26d76668a8dc82b50f88677f797f9deca1d8cf8
hiroemon-m <uvhiro1221511@au.com>
2022-07-26 14:40:18 +0900

    101

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              '今日は何着る？',
+              'あああ',
             ),
             // Text(
             //   '$_counter',
