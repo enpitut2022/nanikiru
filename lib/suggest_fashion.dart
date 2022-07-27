0933092382bc90bb81e5c7043a9cd36f6ca924aa
吉川天斗 <tenten0727@icloud.com>
2022-07-27 11:45:40 +0900

    [103] update suggest_fashion.dart

--- lib/suggest_fashion.dart
+++ lib/suggest_fashion.dart
@@ -6,21 +6,6 @@ import 'dart:async';
 import 'dart:convert';
 import '../utils/constants.dart';
 
-List read_category(String filepath) {
-  // final File file = new File(filepath);
-  // Stream fread = file.openRead();
-  List category_list = [];
-
-  new File(filepath).readAsLines()
-      .then((lines) {
-    for (String line in lines) {
-      category_list.add(line);
-      print('aaa');
-    }
-  });
-  return category_list;
-}
-
 class SuggestFashion extends StatelessWidget{
   Map men_category = Constants().men_category;
   Map women_category = Constants().women_category;
