b7f9fedf7692bbe8ca9c6f90bfb51ff434cfb703
金森透有 <kanamorimichiari@kanamori-no-MacBook-Pro-2041.local>
2022-07-28 09:14:27 +0900

    [401_2]色の登録+服のカテゴリ表示

--- lib/tops.dart
+++ lib/tops.dart
@@ -46,7 +46,7 @@ class CounterStorage {
     final file = await _localFile;
 
     // Write the file
-    return file.writeAsString(counter + ',' + color + '\n' ,mode:FileMode.append);
+    return file.writeAsString("tops" + ',' + counter + ',' + color + '\n' ,mode:FileMode.append);
   }
 }
 
